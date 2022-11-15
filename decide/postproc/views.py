b008c0c1edd810ea8cc2ea26379cd6a1e6f4c44c
pabramber <pabramber@alum.us.es>
2022-11-15 09:29:46 +0100

    feat(init): Implemented dhondt tally
    Implemented the function that calculates the dhondt count. In future updates this function will be integrated with the voting module in order to be able to do the dhondt count.

--- decide/postproc/views.py
+++ decide/postproc/views.py
@@ -16,6 +16,35 @@ class PostProcView(APIView):
         out.sort(key=lambda x: -x['postproc'])
         return Response(out)
 
+    def dhondt(self, options, seats):
+        out = []
+
+        options.sort(key=lambda x: -x['votes'])
+        for opt in options:
+            out.append({
+                **opt,
+                'postproc': 0,
+            })
+
+        for _ in range(seats):
+
+            control_var = 0
+
+            for opt1, opt2 in zip(out, out[1:]):
+                max_value1 = opt1['votes'] / (opt1['postproc'] + 1)
+                max_value2 = opt2['votes'] / (opt2['postproc'] + 1)
+
+                if (max_value1 >= max_value2):
+                    opt1['postproc'] += 1
+                    control_var = 1
+                    break
+
+            if (control_var == 0):
+                out[-1]['postproc'] += 1
+            
+        out.sort(key=lambda x: (-x['postproc'], -x['votes']))
+        return Response(out)
+
     def post(self, request):
         """
          * type: IDENTITY | EQUALITY | WEIGHT
