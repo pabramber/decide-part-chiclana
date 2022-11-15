e078cb54f6774dde4edd560c84f33d0c589f1bdf
pabramber <pabramber@alum.us.es>
2022-11-15 09:40:15 +0100

    feat(update): Implemented dhondt tally
    An enum has been implemented whose values are the possible values for counting, i.e. IDENTITY and DHONDT. This enum will be used for later integration in the voting model, so that it has an attribute that refers to this enum.

--- decide/postproc/models.py
+++ decide/postproc/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from enum import Enum
 
 # Create your models here.
+
+class PostprocTypeEnum(Enum):
+
+    IDENTITY = 'IDENTITY'
+    DHONDT = 'DHONDT'
+
+    @classmethod
+    def choices(cls):
+        return tuple((i.name, i.value) for i in cls)
