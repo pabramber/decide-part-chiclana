1113ebe7f9dacd024a11a40c25131617f1c4798d
Juan Antonio Ortiz Guerra <juanantonioortizguerra@gmail.com>
2022-11-21 11:40:51 +0100

    feat(init): Create user
    Implemented post in api user using a new user serializer

---
+++ decide/ApiUser/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+
+class UserSerializer(serializers.Serializer):
+    username = serializers.CharField(label="Enter username")
+    password = serializers.CharField(label="Enter your password")
+    first_name = serializers.CharField(label="Enter first name")
+    last_name = serializers.CharField(label="Enter last name")
+    email = serializers.EmailField(label="Enter email")
+    is_staff = serializers.BooleanField(label="Enter if this user is staff")
