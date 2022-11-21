4ed98cd3c7ad679b1119820144b79eb8b97f7c29
Juan Antonio Ortiz Guerra <juanantonioortizguerra@gmail.com>
2022-11-21 11:29:57 +0100

    feat(init): Create user Implemented post method in api user, serializer user and  update get

--- decide/ApiUser/views.py
+++ decide/ApiUser/views.py
@@ -2,11 +2,30 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.contrib.auth.models import User
+from .serializers import *
 # Create your views here.
-from django.http import JsonResponse
 class UserListViews(APIView):
+
+    serializer_class = UserSerializer
+
     def get(self,request):
-       userList = User.objects.all()
-       return JsonResponse(list(userList.values()), safe=False)
+       userList = User.objects.all().values()
+       return Response({"Message": "New user added", "user":userList})
+    
+    def post(self, request):
+        print("request data is: ", request.data)
+        serializer_obj = UserSerializer(data=request.data)
+        if(serializer_obj.is_valid()):
+
+            user = User(username=serializer_obj.data.get("username"))
+            user.first_name = serializer_obj.data.get("first_name")
+            user.last_name = serializer_obj.data.get("last_name")
+            user.email = serializer_obj.data.get("email")
+            user.is_staff = serializer_obj.data.get("is_staff")
+            user.set_password(serializer_obj.data.get("password"))
+            user.save()
+            
+        userResponse = User.objects.all().filter(username=request.data["username"]).values()
+        return  Response({"Message": "New user added", "user":userResponse})
 
 
