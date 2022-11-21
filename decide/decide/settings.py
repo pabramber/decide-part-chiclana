192667d7291c2f51d969e5c76ad4c923dd41a0bf
Raul <raulgaro232001@gmail.com>
2022-11-21 20:53:34 +0100

    feat(init): Login with Facebook
    Implemented the possibility of logging in with a Facebook account

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -27,6 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+SOCIAL_AUTH_FACEBOOK_KEY = "1167741833830127"
+SOCIAL_AUTH_FACEBOOK_SECRET = "9f385bef6b16634319fade93cfa7bf5a"
 
 # Application definition
 
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'social_django',
     
 
     
@@ -67,6 +70,7 @@ AUTHENTICATION_BACKENDS = [
     'base.backends.AuthBackend',
     'django.contrib.auth.backends.ModelBackend',
     'allauth.account.auth_backends.AuthenticationBackend',
+    'social_core.backends.facebook.FacebookOAuth2',
 ]
 
 SITE_ID = 1
--- decide/decide/urls.py
+++ decide/decide/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('doc/', schema_view),
     path('gateway/', include('gateway.urls')),
+    path('social-auth/', include('social_django.urls', namespace="social")),
     
 ]
 
