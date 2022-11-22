f5e1c313f5c194059cf109ac061ff9701f10931a
Juan Antonio Ortiz Guerra <juanantonioortizguerra@gmail.com>
2022-11-22 09:24:25 +0100

    test(init): Api user Implemented unit test, only the function get of UserListviews.

--- decide/ApiUser/tests.py
+++ decide/ApiUser/tests.py
@@ -1,3 +1,56 @@
 from django.test import TestCase
+from base.tests import BaseTestCase
+from django.contrib.auth.models import User
 
 # Create your tests here.
+class ApiUserTestCase(TestCase):
+    multi_db = True
+    def setUp(self):
+        super().setUp()
+        username1= "username1"
+        password1="password1"
+        email1="myemail1@gmail.com"
+        first_name1="first_name1"
+        last_name1="last_name1"
+        is_staff1=False
+        self.user = User(username=username1,password=password1,email=email1,first_name=first_name1,
+        last_name=last_name1,is_staff=is_staff1)
+        self.user.save()
+
+        username2= "username2"
+        password2="password2"
+        email2="myemail2@gmail.com"
+        first_name2="first_name2"
+        last_name2="last_name2"
+        is_staff2=True
+        self.user2 = User(username=username2,password=password2,email=email2,first_name=first_name2,
+        last_name=last_name2,is_staff=is_staff2)
+        self.user2.save()
+
+    def tearDown(self):
+        super().tearDown()
+        self.user=None
+
+    def test_list_users(self):
+        url = '/api/user/list'
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data["Message"], "New user added")
+
+        self.assertEqual(response.data["user"][0].get('id'),1)
+        self.assertEqual(response.data["user"][0].get('username'),'username1')
+        self.assertEqual(response.data["user"][0].get('first_name'),'first_name1')
+        self.assertEqual(response.data["user"][0].get('last_name'),'last_name1')
+        self.assertEqual(response.data["user"][0].get('email'),'myemail1@gmail.com')
+        self.assertEqual(response.data["user"][0].get('password'),'password1')
+
+        self.assertEqual(response.data["user"][1].get('id'),2)
+        self.assertEqual(response.data["user"][1].get('username'),'username2')
+        self.assertEqual(response.data["user"][1].get('first_name'),'first_name2')
+        self.assertEqual(response.data["user"][1].get('last_name'),'last_name2')
+        self.assertEqual(response.data["user"][1].get('email'),'myemail2@gmail.com')
+        self.assertEqual(response.data["user"][1].get('password'),'password2')
+        
+
+        
+    
