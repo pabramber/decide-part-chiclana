e5722c37575cae1edc2eae77aa4d78e0671b38c3
alvvegrod <bjuniorz2001@gmail.com>
2022-11-23 19:18:42 +0100

    fix(update):Decide-part-chiclana-3

--- decide/census/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-class Census(models.Model):
-    voting_id = models.PositiveIntegerField()
-    voter_id = models.PositiveIntegerField()
-
-    class Meta:
-        unique_together = (('voting_id', 'voter_id'),)
--- decide/census/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from . import views
-
-
-urlpatterns = [
-    path('', views.CensusCreate.as_view(), name='census_create'),
-    path('<int:voting_id>/', views.CensusDetail.as_view(), name='census_detail'),
-]
