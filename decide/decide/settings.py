1136fc3732752361931a9cb9ab10ab8230dc6866
pabramber <pabramber@alum.us.es>
2022-11-25 15:56:04 +0100

    fix(update): Census does not load on render
    Problem: 500 Server error when DEBUG = False. I'm going to try this: https://stackoverflow.com/a/58481934

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -173,7 +173,7 @@ STATIC_URL = '/static/'
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-DEBUG = False
+
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
