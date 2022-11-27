dc701ebff555bce3b6ee49a99b6e9832985e39b8
Juan Antonio Ortiz Guerra <juanantonioortizguerra@gmail.com>
2022-11-27 12:23:01 +0100

    fix(init): User exist
    Added url so that the user method exists works.

--- decide/ApiUser/urls.py
+++ decide/ApiUser/urls.py
@@ -4,6 +4,6 @@ urlpatterns = [
     path('user/list', UserListViews.as_view(), name='user_list'),
     path('user/<int:id>', UserDetailViews.as_view(), name='user_update'),
     path('user/staff/<int:id>', UserStaffView.as_view(), name='user_is_staff'),
-    path('user/exists', UserExistsView.as_view(), name='user_exists')
+    path('user/exists/<str:username>', UserExistsView.as_view(), name='user_exists')
 
 ]
