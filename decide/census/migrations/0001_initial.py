7b28f0860ab1802ddd3a5913003d2a60bfb9bd45
alvvegrod <bjuniorz2001@gmail.com>
2022-12-12 14:20:54 +0100

    fix(update):decide-part-chiclana-3

--- decide/census/migrations/0001_initial.py
+++ decide/census/migrations/0001_initial.py
@@ -17,19 +17,11 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('voting_id', models.PositiveIntegerField()),
                 ('voter_id', models.PositiveIntegerField()),
-                ('name', models.CharField(max_length=40, null=True)),
-                ('surname', models.CharField(max_length=50, null=True)),
-                ('city', models.CharField(max_length=40, null=True)),
-                ('a_community', models.CharField(max_length=30, null=True)),
-                ('gender', models.CharField(max_length=10, null=True)),
-                ('born_year', models.PositiveIntegerField(null=True)),
-                ('civil_state', models.CharField(max_length=10, null=True)),
-                ('sexuality', models.CharField(max_length=20, null=True)),
-                ('works', models.PositiveIntegerField(null=True)),
+
             ],
         ),
         migrations.AlterUniqueTogether(
             name='census',
-            unique_together={('voting_id', 'voter_id', 'name', 'surname', 'city', 'a_community', 'gender', 'born_year', 'civil_state', 'sexuality', 'works')},
+            unique_together={('voting_id', 'voter_id')},
         ),
     ]
