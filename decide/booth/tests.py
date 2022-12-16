b3f8e26370db7c3dea39321164f4b0df405c19a1
Alberto <albertobemo7@gmail.com>
2022-12-16 19:02:39 +0100

    test(init): Test votaciones disponibles
    Test unitario para comprobar correcta implementación de la nueva función

--- decide/booth/tests.py
+++ decide/booth/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
+from base.tests import BaseTestCase
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+class BoothTestCase(BaseTestCase):
+
+    def setUp(self):
+        super().setUp()
+
+    def tearDown(self):
+        super().tearDown()
+
+    def test_listvoting(self):
+        #Create user, voting and census
+        u = User(username='1234')
+        u.set_password('egc.decide')
+        u.save()
+        data = {'username': '1234', 'password': 'egc.decide'}
+        self.client.post('/authentication/login/', data, format='json')
+        response =self.client.get('/booth/')
+        self.assertEqual(response.status_code, 200)
