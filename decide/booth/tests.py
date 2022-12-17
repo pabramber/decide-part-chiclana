4e211cba4e3260fe5ccdf0b26514840bed6dbf2b
Alberto <albertobemo7@gmail.com>
2022-12-17 02:15:39 +0100

    test(init): Votaciones disponibles
    Test unitario para comprobar el correcto funcionamiento del nuevo path introducido /booth

--- decide/booth/tests.py
+++ decide/booth/tests.py
@@ -1,21 +1,22 @@
 from base.tests import BaseTestCase
 from django.contrib.auth.models import User
+from base import mods
 
 
 class BoothTestCase(BaseTestCase):
 
     def setUp(self):
         super().setUp()
+        mods.mock_query(self.client)
+        u = User(username='1234')
+        u.set_password('egc.decide')
+        u.save()
 
     def tearDown(self):
         super().tearDown()
 
-    def test_listvoting(self):
-        #Create user, voting and census
-        u = User(username='1234')
-        u.set_password('egc.decide')
-        u.save()
+    def test_list_voting(self):
         data = {'username': '1234', 'password': 'egc.decide'}
         self.client.post('/authentication/login/', data, format='json')
         response =self.client.get('/booth/')
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 302)
