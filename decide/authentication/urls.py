0ba25e913e1753b990eeb00330badc67de5ebe01
Alberto <albertobemo7@gmail.com>
2022-12-17 02:28:23 +0100

    fix(end): fallo en /booth
    Corrección de error en el anterior fix

--- decide/authentication/urls.py
+++ decide/authentication/urls.py
@@ -5,8 +5,8 @@ from .views import GetUserView, LogoutView, RegisterView, LoginView, RegisterVie
 
 
 urlpatterns = [
-    path('login-view/', LoginView.as_view()), 
-    path('login/', obtain_auth_token, name="login"),
+    path('login-view/', LoginView.as_view(), name="login"), 
+    path('login/', obtain_auth_token),
     path('logout/', LogoutView.as_view()),
     path('getuser/', GetUserView.as_view()),
     path('register/', RegisterView.as_view()),
